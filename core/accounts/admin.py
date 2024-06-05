e84bfaa4e22f3c2bd2bac8725be3bd363ea3f0f0
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-06-05 12:54:53 +0330

    edit custom admin panel for user

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,4 +1,60 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
+
+
 # Register your models here.
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("email", "first_name", "last_name")
+    readonly_fields = ("created_at", "updated_at")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Personal Info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "created_at", "updated_at")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
+    ordering = ("email",)
+
+
+admin.site.register(User, CustomUserAdmin)
