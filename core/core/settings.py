b100142c83410c8c95d38aafaa133bdec3128184
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-06-05 11:37:17 +0330

    add statics for bootstrap

--- core/core/settings.py
+++ core/core/settings.py
@@ -127,7 +127,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+STATIC_ROOT = "/app/static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static/"),
+]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "/app/media/"
+MEDIAFILES_DIRS = [
+    os.path.join(BASE_DIR, "media/"),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- core/core/urls.py
+++ core/core/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('index.urls')),
     path('accounts/',include('accounts.urls')),
-
 ]
