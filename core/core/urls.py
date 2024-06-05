bf84627aedd3f1f0ac56c184bdb81d3c25a30af4
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-06-05 11:25:15 +0330

    add index view

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('index.urls')),
     path('accounts/',include('accounts.urls')),
-    path('index/',include('index.urls')),
 
 ]
