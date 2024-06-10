0a19d5b2c0420985ad175c6eb3f9b9b6f3860725
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-06-10 18:49:47 +0330

    add jobVision spider

---
+++ core/scraper/management/commands/runjobvisionspider.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from scrapy.crawler import CrawlerProcess
+from spiders.jobvision_spider import JobvisionSpider
+
+class Command(BaseCommand):
+    help = 'Run JobVision Scrapy spider'
+
+    def handle(self, *args, **options):
+        process = CrawlerProcess()
+        process.crawl(JobvisionSpider)
+        process.start()
