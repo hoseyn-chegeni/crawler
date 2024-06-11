7c30423c9ff18ef0f9229897c80ac62c4ab04201
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-06-11 16:53:37 +0330

    check if instance is_expired or not

--- core/spiders/jobvision_spider.py
+++ core/spiders/jobvision_spider.py
@@ -2,7 +2,6 @@ import scrapy
 import json
 from scraper.models import ScrapedData
 
-
 class JobvisionSpider(scrapy.Spider):
     name = "jobvision"
     allowed_domains = ["jobvision.ir"]
@@ -23,71 +22,76 @@ class JobvisionSpider(scrapy.Spider):
                 f"https://candidateapi.jobvision.ir/api/v1/JobPost/Detail?jobPostId={post_id}"
             )
             if job_data:
-                title = job_data.get("title", "N/A")
-                description = job_data.get("description", "N/A")
-                employment_type = job_data.get("workType", {}).get("titleFa", "N/A")
-                location_data = job_data.get("location")
-                if location_data:
-                    city = (
-                        location_data.get("city", {}).get("titleFa", "N/A")
-                        if location_data.get("city")
-                        else "N/A"
-                    )
-                    region = (
-                        location_data.get("region", {}).get("titleFa", "N/A")
-                        if location_data.get("region")
-                        else "N/A"
-                    )
-                    location = f"{city}, {region}"
-                else:
-                    location = "N/A, N/A"
-                gender = job_data.get("gender", {}).get("titleFa", "N/A")
-                minimum_work_experience = job_data.get(
-                    "scoreOfWorkExperienceInJobCategory", {}
-                ).get("expectedValue", "N/A")
-                salary_data = job_data.get("salary")
-                salary = salary_data.get("titleFa", "N/A") if salary_data else "N/A"
-                job_classification = ", ".join(
-                    [
-                        category.get("titleFa", "N/A")
-                        for category in job_data.get("jobCategories", [])
-                    ]
-                )
-                military_service_status_required = job_data.get(
-                    "shouldDoneMilitaryService", False
-                )
-                military_service_status = (
-                    "Required" if military_service_status_required else "Not Required"
-                )
-                hyper_link = response.url
-                company_name = (
-                    job_data.get("company", {}).get("name", {}).get("titleFa", "N/A")
-                )
+                # Check if the job is expired
+                is_expired = job_data.get("isExpired", True)
+                if is_expired == False:
 
-                skills_list = job_data.get("softwareRequirements") or []
-                skills = ", ".join(
-                    [
-                        f"{skill.get('software', {}).get('titleFa', 'N/A')} ({skill.get('skill', {}).get('titleFa', 'N/A')})"
-                        for skill in skills_list
-                    ]
-                )
 
-                ScrapedData.objects.create(
-                    title=title,
-                    description=description,
-                    employment_type=employment_type,
-                    location=location,
-                    gender=gender,
-                    minimum_work_experience=minimum_work_experience,
-                    salary=salary,
-                    skills=skills,
-                    job_classification=job_classification,
-                    military_service_status=military_service_status,
-                    hyper_link=hyper_link,
-                    company_name=company_name,
-                    web_app="JobVision",
-                )
-                self.logger.info(f"Saved job post: {title}")
+                    title = job_data.get("title", "N/A")
+                    description = job_data.get("description", "N/A")
+                    employment_type = job_data.get("workType", {}).get("titleFa", "N/A")
+                    location_data = job_data.get("location")
+                    if location_data:
+                        city = (
+                            location_data.get("city", {}).get("titleFa", "N/A")
+                            if location_data.get("city")
+                            else "N/A"
+                        )
+                        region = (
+                            location_data.get("region", {}).get("titleFa", "N/A")
+                            if location_data.get("region")
+                            else "N/A"
+                        )
+                        location = f"{city}, {region}"
+                    else:
+                        location = "N/A, N/A"
+                    gender = job_data.get("gender", {}).get("titleFa", "N/A")
+                    minimum_work_experience = job_data.get(
+                        "scoreOfWorkExperienceInJobCategory", {}
+                    ).get("expectedValue", "N/A")
+                    salary_data = job_data.get("salary")
+                    salary = salary_data.get("titleFa", "N/A") if salary_data else "N/A"
+                    job_classification = ", ".join(
+                        [
+                            category.get("titleFa", "N/A")
+                            for category in job_data.get("jobCategories", [])
+                        ]
+                    )
+                    military_service_status_required = job_data.get(
+                        "shouldDoneMilitaryService", False
+                    )
+                    military_service_status = (
+                        "Required" if military_service_status_required else "Not Required"
+                    )
+                    hyper_link = response.url
+                    company_name = (
+                        job_data.get("company", {}).get("name", {}).get("titleFa", "N/A")
+                    )
+
+                    skills_list = job_data.get("softwareRequirements") or []
+                    skills = ", ".join(
+                        [
+                            f"{skill.get('software', {}).get('titleFa', 'N/A')} ({skill.get('skill', {}).get('titleFa', 'N/A')})"
+                            for skill in skills_list
+                        ]
+                    )
+
+                    ScrapedData.objects.create(
+                        title=title,
+                        description=description,
+                        employment_type=employment_type,
+                        location=location,
+                        gender=gender,
+                        minimum_work_experience=minimum_work_experience,
+                        salary=salary,
+                        skills=skills,
+                        job_classification=job_classification,
+                        military_service_status=military_service_status,
+                        hyper_link=hyper_link,
+                        company_name=company_name,
+                        web_app="JobVision",
+                    )
+                    self.logger.info(f"Saved job post: {title}")
 
         for i in range(700001, 770000):
             next_page = f"https://jobvision.ir/jobs/{i}/"
