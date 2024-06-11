fdcbf1cb3d53d0b92fc5c754ee578ad38acef761
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-06-11 10:28:35 +0330

    add custom filter to scraped data filter

---
+++ core/scraper/api/v1/filters.py
@@ -0,0 +1,35 @@
+import django_filters
+from ...models import ScrapedData
+
+class ScrapedDataFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(lookup_expr='icontains')
+    description = django_filters.CharFilter(lookup_expr='icontains')
+    employment_type = django_filters.CharFilter(lookup_expr='icontains')
+    location = django_filters.CharFilter(lookup_expr='icontains')
+    gender = django_filters.CharFilter(lookup_expr='icontains')
+    minimum_work_experience = django_filters.CharFilter(lookup_expr='icontains')
+    salary = django_filters.CharFilter(lookup_expr='icontains')
+    skills = django_filters.CharFilter(lookup_expr='icontains')
+    job_classification = django_filters.CharFilter(lookup_expr='icontains')
+    military_service_status = django_filters.CharFilter(lookup_expr='icontains')
+    hyper_link = django_filters.CharFilter(lookup_expr='icontains')
+    company_name = django_filters.CharFilter(lookup_expr='icontains')
+    web_app = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = ScrapedData
+        fields = [
+            "title",
+            "description",
+            "employment_type",
+            "location",
+            "gender",
+            "minimum_work_experience",
+            "salary",
+            "skills",
+            "job_classification",
+            "military_service_status",
+            "hyper_link",
+            "company_name",
+            "web_app",
+        ]
--- core/scraper/api/v1/views.py
+++ core/scraper/api/v1/views.py
@@ -4,23 +4,12 @@ from rest_framework.generics import ListAPIView
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .pagination import LargeResultsSetPagination
+from .filters import ScrapedDataFilter
 
 class ScrapedDataListView(ListAPIView):
     serializer_class = ScrapedDataSerializer
     queryset = ScrapedData.objects.all()
     filter_backends = [DjangoFilterBackend, OrderingFilter]
-    filterset_fields = [
-        "id",
-        "title",
-        "employment_type",
-        "location",
-        "gender",
-        "salary",
-        "skills",
-        "job_classification",
-        "military_service_status",
-        "company_name",
-        "web_app",
-    ]
+    filterset_class = ScrapedDataFilter
     ordering_fields = ["id", "created_at"]
     pagination_class = LargeResultsSetPagination
