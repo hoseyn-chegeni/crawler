2b4434060534f229a8ef207013581e074a4ef91a
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-06-12 09:57:42 +0330

    change swagger url route

--- core/core/swagger.py
+++ core/core/swagger.py
@@ -16,7 +16,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path(
-        "swagger/",
+        "",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
